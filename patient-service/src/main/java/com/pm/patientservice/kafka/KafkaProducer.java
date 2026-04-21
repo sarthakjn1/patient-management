6229ea886ed92cc71136ebebb2b33c153bcdafb8
Sarthak Nagapurkar <nsarthak99@gmail.com>
2026-04-21 11:32:32 +0530

    Call sendEvent for KafkaProducer from Patient Service.

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,14 +1,34 @@
 package com.pm.patientservice.kafka;
 
+import com.pm.patientservice.model.Patient;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class KafkaProducer {
 
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try{
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
     }
 }
